15b32683af398a8727268c575e36af2433d7c928
Cristian C. Gorrin <cristian_gorrin@live.com>
2017-11-08 14:38:20 +0100

    Add support for cron jobs

---
+++ web_server/lib/conf/cron.php
@@ -0,0 +1,12 @@
+<?php
+namespace StudentCheckIn;
+abstract class ConfCron {
+    const HOURLY   = 0;
+    const DALY     = 1;
+    const MONTHLY  = 2;
+
+    public static $run_all = false;
+    public static $scripts = array(
+        // [file_name] => self::[HOURLY | DALY | MONTHLY]
+    );
+}
---
+++ web_server/lib/cron/run_all.php
@@ -0,0 +1,8 @@
+<?php
+namespace StudentCheckIn;
+
+require_once '../autoload.php';
+
+ConfCron::$run_all = true;
+
+require './manage.php';
