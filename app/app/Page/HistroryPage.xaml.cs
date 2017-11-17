27c84a97080408049b27a7982f0e785e431cee73
Cristian C. Gorrin <cristian_gorrin@live.com>
2017-11-17 14:02:13 +0100

    Add histrory page

--- app/app/Page/HistroryPage.xaml.cs
+++ app/app/Page/HistroryPage.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using app_lib;
+using app_lib.Datasource;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -10,11 +12,20 @@ using Xamarin.Forms.Xaml;
 namespace app.Page
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class HistroryPage : ContentPage
-	{
-		public HistroryPage ()
-		{
+	public partial class HistroryPage : ContentPage	{
+        private List<IHistroryEntity> m_list_history;
+
+        public HistroryPage () {
 			InitializeComponent ();
-		}
-	}
+
+            m_list_history                = Datasource.GetListHistrory();
+            list_view_history.ItemsSource = m_list_history;
+
+            Title = m_list_history.Count.ToString();
+        }
+
+        private void ListItemSelected(object sender, SelectedItemChangedEventArgs e) {
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
 }
