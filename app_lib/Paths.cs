c0e32b5f4a2296e2cb5b3d0cd901d288b1033a50
Cristian C. Gorrin <cristian_gorrin@live.com>
2017-11-22 21:16:55 +0100

    Add file api to app_lib

---
+++ app_lib/Paths.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace app_lib {
+    public static class Paths {
+        public static string SessionFile { get; set; }
+        public static Func<string, StreamType, Stream> GetStream { get; set; }
+        public static Func<string, bool> FileExist { get; set; }
+        public static Func<string, bool> DeleteFile { get; set; }
+
+        static Paths() {
+            switch (Device.RuntimePlatform) {
+                case Device.Android:
+                    SessionFile = "session.json";
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        public enum StreamType { Input, Output }
+    }
+}
