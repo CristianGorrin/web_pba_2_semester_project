16dc63ba9908ec23f54bad19e6ff3906bc853811
Cristian C. Gorrin <cristian_gorrin@live.com>
2017-11-24 16:48:28 +0100

    Geolocator servic

---
+++ app_lib/Geolocator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
+using System.Threading.Tasks;
+
+namespace app_lib {
+    public static class Geolocator {
+        public static async Task<Position> GetCurrentLocation() {
+            try {
+                var locator             = CrossGeolocator.Current;
+                locator.DesiredAccuracy = 10;
+
+                return await locator.GetLastKnownLocationAsync();
+            } catch (Exception ex) {
+                return null;
+            }
+        }
+    }
+}
