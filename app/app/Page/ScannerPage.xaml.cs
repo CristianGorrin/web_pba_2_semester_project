dd202fb26e661ea23f5a2e8e88d46ed396da394d
Cristian C. Gorrin <cristian_gorrin@live.com>
2017-12-04 13:05:48 +0100

    Update the gui based on user feedback

--- app/app/Page/ScannerPage.xaml.cs
+++ app/app/Page/ScannerPage.xaml.cs
@@ -27,6 +27,25 @@ namespace app.Page {
                 VerticalOptions   = LayoutOptions.FillAndExpand
             };
 
+            var lab      = new Label();
+            var grid_lab = new Grid() {
+                VerticalOptions   = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                RowDefinitions    = new RowDefinitionCollection() {
+                    new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) },
+                    new RowDefinition() { Height = new GridLength(40, GridUnitType.Absolute) }
+                }
+            };
+
+            lab.Text                    = "Scanned the qr code to check in";
+            lab.HorizontalTextAlignment = TextAlignment.Center;
+            lab.VerticalTextAlignment   = TextAlignment.Center;
+            lab.BackgroundColor         = new Color(255, 255, 255, 0.3);
+            lab.FontSize                = 24;
+            lab.SetValue(Grid.RowProperty, 1);
+
+            grid_lab.Children.Add(lab);
+
             m_zxing.OnScanResult += DoResult;
 
             var grid = new Grid {
@@ -34,6 +53,7 @@ namespace app.Page {
                 HorizontalOptions = LayoutOptions.FillAndExpand
             };
             grid.Children.Add(m_zxing);
+            grid.Children.Add(grid_lab);
 
             Content = grid;
         }
